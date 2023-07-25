4199d0a65a1ce666f59227f2c6557d73f5021119
Masayuki Igawa <masayuki@igawa.io>
2023-07-26 08:55:27 +0900

    Add answer for ex1

--- lesson2/ex1.py
+++ lesson2/ex1.py
@@ -4,17 +4,30 @@
 
 # 1. Create a format string to display the name and age of a person.
 
-# INSERT YOUR CODE HERE
+name: str = "Masayuki"
+age: int = 47
+print("Name: {}, Age: {}".format(name, age))
+print(f"Name: {name}, Age: {age}")
 
 # 2. Print version with it's corresponding upstream codename,
 #    and use padding aligned format to left, centre and right.
 
-# INSERT YOUR CODE HERE
+versions: dict[str, str] = {"13": "Queens", "16": "Train", "17": "Wallaby"}
+for version, codename in versions.items():
+    print(f"{version: <10} - {codename}")
+    print(f"{version: ^10} - {codename}")
+    print(f"{version: >10} - {codename}")
 
 # 3. Show different representations of an integer.
 
-# INSERT YOUR CODE HERE
+types: list[str] = ['08b', 'c', 'd', 'o', 'x', 'n', '']
+for t in types:
+    print(t + ": " + format(10, t))
+#    print("{:b}".format(10))
+
 
 # 4. Format a floating-point number with fixed precision.
 
-# INSERT YOUR CODE HERE
+ftypes: list[str] = ['e', 'E', 'f', 'F', 'g', 'G', 'n', '%', '']
+for t in ftypes:
+    print(t + ": " + format(10, t))
