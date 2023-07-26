25bbb2c96f87fac75b65b53d58aae630ed843425
Masayuki Igawa <masayuki@igawa.io>
2023-07-26 09:58:13 +0900

    Add ex4 answer

--- lesson2/ex4.py
+++ lesson2/ex4.py
@@ -5,12 +5,16 @@
 # 1. Print the formatted keys and values of the dictionary.
 #    versions: dict[str, int] = {'Stein': 15, 'Train': 16, 'Wallaby': 17}
 
-# INSERT YOUR CODE HERE
+versions: dict[str, int] = {'Stein': 15, 'Train': 16, 'Wallaby': 17}
+for k, v in versions.items():
+    print("Codename: {0}, version: {1}".format(k, v))
 
 # 2. Print {} around the version numbers.
-
-# INSERT YOUR CODE HERE
+for k, v in versions.items():
+    print("Codename: {0}, version: {{{1}}}".format(k, v))
 
 # 3. Print numbers in decimal, byte and hexadecimal form.
 
-# INSERT YOUR CODE HERE
+for k, v in versions.items():
+    print("Codename: {0}, version(decimal): {1:d}, (byte): {1:c},"
+          " (hexdecimal): {1:x}".format(k, v))
