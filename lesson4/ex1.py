f022461e6dd88f0e4df981faccfc11dd26a5e169
Masayuki Igawa <masayuki@igawa.io>
2023-08-23 09:01:49 +0900

    Add lesson4/ex1 answer

--- lesson4/ex1.py
+++ lesson4/ex1.py
@@ -6,40 +6,54 @@
 # apple: 10
 # orange: 2
 
-# INSERT YOUR CODE HERE
+fruits: dict[str, int] = {"apple": 10, "orange": 2}
 
 # 2. Add banana to the dictionary and set it's value to 3
 
-# INSERT YOUR CODE HERE
+fruits['banana'] = 3
 
 # 3. Add mandarin using dictionary method .update()
 
-# INSERT YOUR CODE HERE
+fruits.update({"mandarin": 1})
+print(fruits)
 
 # 4. Remove the mandarin from the dictionary
 
-# INSERT YOUR CODE HERE
+del fruits['mandarin']
+print(fruits)
 
 # 5. Add 10 more apples and remove 2 bananas
 
-# INSERT YOUR CODE HERE
+fruits.update({"apple": fruits['apple'] + 10})
+fruits.update({"banana": fruits['banana'] - 2})
+print(fruits)
 
 # 6. Remove 'apple' from the dictionary using .pop()
 #    and save it's value into a variable 'apples'
 
-# INSERT YOUR CODE HERE
+apples: int = fruits.pop('apple')
+print(fruits, apples)
 
 # 7. Remove 'mandarin' from the dictionary using .pop()
 #    and save it's value into a variable 'mandarin'
 #    if 'mandarin' doesn't exist set the variable to 0
 
-# INSERT YOUR CODE HERE
+mandarin: int
+try:
+    mandarin = fruits.pop('mandarin')
+except KeyError:
+    # ignore
+    mandarin = 0
+
+print(fruits, mandarin)
 
 # 8. Remove last item from the dictionary using .popitem()
 #    and save it's value into variable 'last'
 
-# INSERT YOUR CODE HERE
+last: tuple[str, int] = fruits.popitem()
+print(last)
 
 # 9. Check if the 'banana' is in the fruits dictionary.
 
-# INSERT YOUR CODE HERE
+print(fruits)
+'banana' in fruits
