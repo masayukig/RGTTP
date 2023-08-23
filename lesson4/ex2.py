bb011934543edcb50158a2f388eb6adb79071677
Masayuki Igawa <masayuki@igawa.io>
2023-08-23 09:22:41 +0900

    Add lesson4/ex2 answer

--- lesson4/ex2.py
+++ lesson4/ex2.py
@@ -7,7 +7,7 @@
 # orange: 2
 # banana: 3
 
-# INSERT YOUR CODE HERE
+fruits: dict[str, int] = {"apple": 10, "orange": 2, "banana": 3}
 
 # 2. Iterate over fruits in fruits dictionary using for loop,
 #    print using f-strings:
@@ -15,25 +15,37 @@
 #    orange: 2
 #    banana: 3
 
-# INSERT YOUR CODE HERE
+for i, j in fruits.items():
+    print(f'{i}: {j}')
 
 # 2. Iterate over the keys in 'fruits' dictionary
 
-# INSERT YOUR CODE HERE
+for i in fruits:
+    print(f'{i}')
 
 # 3. Iterate over the values in dictionary
 
-# INSERT YOUR CODE HERE
+for value in fruits.values():
+    print(f"{value}")
 
 # 4. Access value banana using .get() method
 
-# INSERT YOUR CODE HERE
+print(fruits.get("banana"))
 
 # 5. Access value mandarin using .get() method,
 #    if 'mandarin' doesn't exist, return 0
 
-# INSERT YOUR CODE HERE
+mandarin: int | None
+mandarin = fruits.get("mandarin")
+
+mandarin = fruits.get("mandarin")
+if mandarin is None:
+    mandarin = 0
+print(mandarin)
 
 # 6. Remove all items from the dictionary
 
-# INSERT YOUR CODE HERE
+while (fruits):
+    print(fruits.popitem())
+
+print(fruits)
