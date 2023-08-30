dd2cce594b47dc20fae6c310f5f1ed98dfebe900
Masayuki Igawa <masayuki@igawa.io>
2023-08-30 11:51:12 +0900

    Add lesson5/ex2 answer

--- lesson5/ex2.py
+++ lesson5/ex2.py
@@ -4,22 +4,48 @@
 
 # 1. Write a while true loop to print "Forever" forever
 
-# INSERT YOUR CODE HERE
+# while True:
+#     print("Forever")
 
 # 2. Write a while loop to print numbers from 0 to 42
 
-# INSERT YOUR CODE HERE
+n: int = 0
+while (n <= 42):
+    print(n)
+    n += 1
 
 # 3. Write a while true loop to print numbers from 0 to 42
-
-# INSERT YOUR CODE HERE
+n: int = 0
+while True:
+    if (n > 42):
+        break
+    print(n)
+    n += 1
 
 # 4. Write a while true loop to print numbers from 0 to 45, and instead
 #    of 42, print "I am 42!" break at number 45.
+n: int = 0
+while True:
+    if (n > 45):
+        break
+    elif (n == 42):
+        print("I am 42!")
+    else:
+        print(n)
+    n += 1
 
-# INSERT YOUR CODE HERE
 
 # 5. Write a while-else loop to count to 2, and after that print
 #    "It's my turn now!" using else statement.
 
-# INSERT YOUR CODE HERE
+n: int = 0
+while True:
+    if (n > 45):
+        break
+    elif (n == 42):
+        print("I am 42!")
+    else:
+        print(n)
+    n += 1
+else:
+    print("It's my turn now!")
