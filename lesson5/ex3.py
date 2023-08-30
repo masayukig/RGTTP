86eb043f9361dfe688129742f178dce3e7a589e5
Masayuki Igawa <masayuki@igawa.io>
2023-08-30 11:58:15 +0900

    Add lesson5/ex3 answer

--- lesson5/ex3.py
+++ lesson5/ex3.py
@@ -4,36 +4,50 @@
 
 # 1. Write a for loop to iterate over list: [1, 2, 3, 'a', 'b', 'c']
 
-# INSERT YOUR CODE HERE
+for i in [1, 2, 3, 'a', 'b', 'c']:
+    print(i)
 
 # 2. Write for loop to print each character in word "orange"
 
-# INSERT YOUR CODE HERE
+for c in "orange":
+    print(c)
+
 
 # 3. Using following shopping list:
 # shopping_list: ['orange', 'banana', 'mandarin']
 # Print "Note to self, buy: " and then iterate
 # over each element in the list
 
-# INSERT YOUR CODE HERE
+shopping_list: list[str] = ['orange', 'banana', 'mandarin']
 
 # 4. Write for loop using range to print numbers from 0 to 10
+# print("Note to self, buy: ")
+# for index in range(0, len(shopping_list)):
+#     print(f"{index}, {shopping_list[index]}")
 
-# INSERT YOUR CODE HERE
+print("Note to self, buy: ")
+for index, item in enumerate(shopping_list, start=1):
+    print(f"{index}, {item}")
 
 # 5. Write for loop using range to print numbers from 10 to 20
 
-# INSERT YOUR CODE HERE
+for i in range(10, 21):
+    print(i)
 
 # 6. Write for loop using range to print even numbers from 10 to 20
 
-# INSERT YOUR CODE HERE
+for i in range(10, 21):
+    if (i % 2 == 0):
+        print(i)
+
 
 # 7. Write for loop using range, to print every 5 numbers
 #    down from 100 to 0
 
-# INSERT YOUR CODE HERE
+for i in range(100, 0, -5):
+    print(i)
 
 # 8. Write for loop using enumerate to print element and it's index
 
-# INSERT YOUR CODE HERE
+for i in enumerate(shopping_list):
+    print(i[0], i[1])
